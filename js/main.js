ba99161016a822537aa5f8c43372c24c068cd5ed
Lucas Faustman <lucasfaustman@gmail.com>
2022-05-16 19:42:54 -0700

    Added OOP practices

--- js/main.js
+++ js/main.js
@@ -2,33 +2,155 @@
 // when a user gets three in a row, whether its X's or O's, I want a prompt to come up that says User 1 or user 2 is the winner
 
 
+// without OOP
 
 
-let boxes = document.querySelectorAll('section')
-boxes.forEach(box => box.addEventListener('click', makeChoice))
-let counter = 0
-let playing = true
+
+// let boxes = document.querySelectorAll('section')
+// boxes.forEach(box => box.addEventListener('click', makeChoice))
+// let counter = 0
+// let playing = true
 
 
-function makeChoice (event){
-    if (counter >= 9 || playing === false){
-       counter = null
+// function makeChoice (event){
+//     if (counter >= 9 || playing === false){
+//        counter = null
+//        alert('The game is over')
+//    } else if (event.target.innerText === 'X' || event.target.innerText === 'O' && counter <= 9 && playing == true){
+//        alert('Please pick a box that has not been picked')
+//    }
+//     else if (counter === 0 && playing || counter % 2 === 0 && playing) {
+//         event.target.innerText = 'X'
+//         counter ++
+//         checkWinner()
+//     } else if (counter % 2 != 0 && playing) {
+//         event.target.innerText = 'O'
+//         counter ++
+//         checkWinner()
+//     } 
+// }
+
+// function checkWinner() {
+//     if ((document.querySelector('#boxone').innerText === 'X') &&
+//     (document.querySelector('#boxtwo').innerText === 'X') &&
+//     (document.querySelector('#boxthree').innerText === 'X')
+//     ||
+//     (document.querySelector('#boxfour').innerText === 'X') &&
+//     (document.querySelector('#boxfive').innerText === 'X') &&
+//     (document.querySelector('#boxsix').innerText === 'X')
+//     ||
+//     (document.querySelector('#boxseven').innerText === 'X') &&
+//     (document.querySelector('#boxeight').innerText === 'X') &&
+//     (document.querySelector('#boxnine').innerText === 'X')
+//     ||
+
+
+//     (document.querySelector('#boxone').innerText === 'X') &&
+//     (document.querySelector('#boxfour').innerText === 'X') &&
+//     (document.querySelector('#boxseven').innerText === 'X')
+//     ||
+//     (document.querySelector('#boxtwo').innerText === 'X') &&
+//     (document.querySelector('#boxfive').innerText === 'X') &&
+//     (document.querySelector('#boxeight').innerText === 'X')
+//     ||
+//     (document.querySelector('#boxthree').innerText === 'X') &&
+//     (document.querySelector('#boxsix').innerText === 'X') &&
+//     (document.querySelector('#boxnine').innerText === 'X')
+//     ||
+
+//     (document.querySelector('#boxthree').innerText === 'X') &&
+//     (document.querySelector('#boxfive').innerText === 'X') &&
+//     (document.querySelector('#boxseven').innerText === 'X')
+//     ||
+//     (document.querySelector('#boxone').innerText === 'X') &&
+//     (document.querySelector('#boxfive').innerText === 'X') &&
+//     (document.querySelector('#boxnine').innerText === 'X')
+//     ) {
+//  alert ('X player wins!')
+// playing = false
+//    } else if (
+//     (document.querySelector('#boxone').innerText === 'O') &&
+//     (document.querySelector('#boxtwo').innerText === 'O') &&
+//     (document.querySelector('#boxthree').innerText === 'O')
+//     ||
+//     (document.querySelector('#boxfour').innerText === 'O') &&
+//     (document.querySelector('#boxfive').innerText === 'O') &&
+//     (document.querySelector('#boxsix').innerText === 'O')
+//     ||
+//     (document.querySelector('#boxseven').innerText === 'O') &&
+//     (document.querySelector('#boxeight').innerText === 'O') &&
+//     (document.querySelector('#boxnine').innerText === 'O')
+//     ||
+
+
+//     (document.querySelector('#boxone').innerText === 'O') &&
+//     (document.querySelector('#boxfour').innerText === 'O') &&
+//     (document.querySelector('#boxseven').innerText === 'O')
+//     ||
+//     (document.querySelector('#boxtwo').innerText === 'O') &&
+//     (document.querySelector('#boxfive').innerText === 'O') &&
+//     (document.querySelector('#boxeight').innerText === 'O')
+//     ||
+//     (document.querySelector('#boxthree').innerText === 'O') &&
+//     (document.querySelector('#boxsix').innerText === 'O') &&
+//     (document.querySelector('#boxnine').innerText === 'O')
+//     ||
+
+//     (document.querySelector('#boxthree').innerText === 'O') &&
+//     (document.querySelector('#boxfive').innerText === 'O') &&
+//     (document.querySelector('#boxseven').innerText === 'O')
+//     ||
+//     (document.querySelector('#boxone').innerText === 'O') &&
+//     (document.querySelector('#boxfive').innerText === 'O') &&
+//     (document.querySelector('#boxnine').innerText === 'O')
+
+
+//    ) {
+//        alert('O Player wins!')
+//    } else if (counter >= 9)
+//     alert('It is a draw')
+// }
+
+
+
+
+// let winConditions = [[0,1,2], [3,4,5] [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,9] , [2,4,6] ]
+// let board = ['', '', '', '', '', '', '', '', '', ]
+
+
+
+// with OOP
+
+class TicTacToeGame {
+    constructor () {
+
+this.counter = 0
+this.playing = true
+    }
+
+addListeners() {
+    this.boxes = document.querySelectorAll('section').forEach(box => box.addEventListener('click', this.makeChoice.bind(this)))
+}
+
+makeChoice (event){
+    if (this.counter >= 9 || this.playing === false){
+       this.counter = null
        alert('The game is over')
-   } else if (event.target.innerText === 'X' || event.target.innerText === 'O' && counter <= 9 && playing == true){
+   } else if (event.target.innerText === 'X' || event.target.innerText === 'O' && this.counter <= 9 && this.playing == true){
        alert('Please pick a box that has not been picked')
    }
-    else if (counter === 0 && playing || counter % 2 === 0 && playing) {
+    else if (this.counter === 0 && this.playing || this.counter % 2 === 0 && this.playing) {
         event.target.innerText = 'X'
-        counter ++
-        checkWinner()
-    } else if (counter % 2 != 0 && playing) {
+        this.counter ++
+        this.checkWinner()
+    } else if (this.counter % 2 != 0 && this.playing) {
         event.target.innerText = 'O'
-        counter ++
-        checkWinner()
+        this.counter ++
+        this.checkWinner()
     } 
 }
 
-function checkWinner() {
+checkWinner() {
     if ((document.querySelector('#boxone').innerText === 'X') &&
     (document.querySelector('#boxtwo').innerText === 'X') &&
     (document.querySelector('#boxthree').innerText === 'X')
@@ -65,7 +187,7 @@ function checkWinner() {
     (document.querySelector('#boxnine').innerText === 'X')
     ) {
  alert ('X player wins!')
-playing = false
+this.playing = false
    } else if (
     (document.querySelector('#boxone').innerText === 'O') &&
     (document.querySelector('#boxtwo').innerText === 'O') &&
@@ -105,12 +227,10 @@ playing = false
 
    ) {
        alert('O Player wins!')
-   } else if (counter >= 9)
+   } else if (this.counter >= 9)
     alert('It is a draw')
 }
+}
 
-
-
-
-// let winConditions = [[0,1,2], [3,4,5] [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,9] , [2,4,6] ]
-// let board = ['', '', '', '', '', '', '', '', '', ]
+let game = new TicTacToeGame
+game.addListeners()
